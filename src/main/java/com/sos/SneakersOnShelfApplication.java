b2401eb17a02cd6c90e796abca1b4d895dabceeb
Vũ Long <vu6u07@gmail.com>
2022-09-26 21:31:46 +0700

    Tạo spring boot project

---
+++ src/main/java/com/sos/SneakersOnShelfApplication.java
@@ -0,0 +1,13 @@
+package com.sos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SneakersOnShelfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SneakersOnShelfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sos/SneakersOnShelfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SneakersOnShelfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
