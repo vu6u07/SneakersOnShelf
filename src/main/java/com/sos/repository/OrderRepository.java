b236859a4dd29db3a23022cfab9b8b95c6c0dee0
Vũ Long <vu6u07@gmail.com>
2022-11-06 12:51:39 +0700

    Update xem tất cả đơn hàng đã đặt và thống kê

--- src/main/java/com/sos/repository/OrderRepository.java
+++ src/main/java/com/sos/repository/OrderRepository.java
@@ -4,11 +4,14 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.sos.dto.CartItemDTO;
+import com.sos.dto.PurchaseDTO;
 import com.sos.dto.PurchaseInfoDTO;
 import com.sos.entity.Order;
 
@@ -17,13 +20,16 @@ public interface OrderRepository extends JpaRepository<Order, UUID> {
 
 	@Query(value = "SELECT new com.sos.dto.CartItemDTO(o.id, o.quantity, o.productDetail.product.id, o.productDetail.id, o.productDetail.product.name, o.productDetail.size, o.productDetail.product.productImage.image, o.price, r) FROM OrderItem o LEFT JOIN o.rate r WHERE o.order.id = :id")
 	List<CartItemDTO> findAllPurchaseItemDTO(UUID id);
-	
-	//Anonymous
+
+	// Anonymous
 	@Query(value = "SELECT new com.sos.dto.PurchaseInfoDTO(o.id, o.token, o.discount, o.surcharge, o.total, o.description, o.orderStatus, o.paymentStatus, o.paymentMethod, o.createDate, o.updateDate) FROM Order o WHERE o.id = :id AND o.token = :token")
 	Optional<PurchaseInfoDTO> findPurchaseInfoDTO(UUID id, String token);
-	
-	//Logged User
+
+	// Logged User
 	@Query(value = "SELECT new com.sos.dto.PurchaseInfoDTO(o.id, o.token, o.discount, o.surcharge, o.total, o.description, o.orderStatus, o.paymentStatus, o.paymentMethod, o.createDate, o.updateDate) FROM Order o WHERE o.id = :id AND o.customerInfo.account.id = :accountId")
 	Optional<PurchaseInfoDTO> findPurchaseInfoDTO(UUID id, int accountId);
+
+	@Query(value = "SELECT new com.sos.dto.PurchaseDTO(o.id, COUNT(oi.quantity), o.total, o.orderStatus, o.paymentStatus, o.createDate, o.updateDate) FROM Order o JOIN o.orderItems oi WHERE o.customerInfo.account.id = :accountId")
+	Page<PurchaseDTO> findAllPurchaseDTOByAccountId(int accountId, Pageable pageable);
 	
 }
