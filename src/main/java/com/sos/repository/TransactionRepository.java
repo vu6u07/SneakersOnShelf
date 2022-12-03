e93dc2e81dca1d0dd94fafb8578d058ea835a3cc
Vũ Long <vu6u07@gmail.com>
2022-12-03 16:48:50 +0700

    Update TransactionRepository.java

--- src/main/java/com/sos/repository/TransactionRepository.java
+++ src/main/java/com/sos/repository/TransactionRepository.java
@@ -19,14 +19,14 @@ import com.sos.entity.Transaction;
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Integer> {
 
-	@Query(value = "SELECT new com.sos.dto.TransactionDTO(t.id, a.fullname, t.transactionType, t.transactionStatus, t.paymentMethod, t.amount, t.createDate, t.updateDate) FROM Transaction t JOIN t.staff a WHERE t.order.id = :id")
+	@Query(value = "SELECT new com.sos.dto.TransactionDTO(t.id, COALESCE(a.fullname, ''), t.transactionType, t.transactionStatus, t.paymentMethod, t.amount, t.createDate, t.updateDate) FROM Transaction t LEFT JOIN t.staff a WHERE t.order.id = :id")
 	List<TransactionDTO> findAllTransactionDTOByOrderId(String id);
 
-	@Query(value = "SELECT new com.sos.dto.TransactionDTO(t.id, a.fullname, o.id, t.transactionType, t.transactionStatus, t.paymentMethod, t.amount, t.createDate, t.updateDate) FROM Transaction t JOIN t.staff a JOIN t.order o WHERE (:query IS NULL OR (o.id LIKE :query OR a.fullname LIKE :query)) AND (:transactionType IS NULL OR t.transactionType = :transactionType) AND (:paymentMethod IS NULL OR t.paymentMethod = :paymentMethod)")
+	@Query(value = "SELECT new com.sos.dto.TransactionDTO(t.id, COALESCE(a.fullname, ''), o.id, t.transactionType, t.transactionStatus, t.paymentMethod, t.amount, t.createDate, t.updateDate) FROM Transaction t LEFT JOIN t.staff a JOIN t.order o WHERE (:query IS NULL OR (o.id LIKE :query OR a.fullname LIKE :query)) AND (:transactionType IS NULL OR t.transactionType = :transactionType) AND (:paymentMethod IS NULL OR t.paymentMethod = :paymentMethod)")
 	Page<TransactionDTO> findTransactions(String query, TransactionType transactionType, PaymentMethod paymentMethod,
 			Pageable pageable);
 	
-	@Query(value = "SELECT new com.sos.dto.TransactionDTO(t.id, a.fullname, o.id, t.transactionType, t.transactionStatus, t.paymentMethod, t.amount, t.createDate, t.updateDate) FROM Transaction t JOIN t.staff a JOIN t.order o WHERE t.id = :id AND t.transactionStatus = :transactionStatus")
+	@Query(value = "SELECT new com.sos.dto.TransactionDTO(t.id, COALESCE(a.fullname, ''), o.id, t.transactionType, t.transactionStatus, t.paymentMethod, t.amount, t.createDate, t.updateDate) FROM Transaction t LEFT JOIN t.staff a JOIN t.order o WHERE t.id = :id AND t.transactionStatus = :transactionStatus")
 	Optional<TransactionDTO> findTransactionDTOById(int id, TransactionStatus transactionStatus);
 	
 	@Modifying
